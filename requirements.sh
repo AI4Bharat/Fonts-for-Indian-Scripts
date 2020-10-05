f86b1b093315baba32a45616fd48baf418b44312
S7309 <36332337+S7309@users.noreply.github.com>
2020-10-05 11:04:18 +0530

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+pip3 install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+sudo pip3 install Pillow
+sudo apt-get install libfreetype6-dev libharfbuzz-dev libfribidi-dev gtk-doc-tools
+sudo apt update
+sudo apt install libraqm-dev
+sudo ldconfig
+sudo apt install fonts-indic
