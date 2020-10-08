da0c689ce39b37ad2e36c2957fae7d4596a8a1e7
Ishvinder Sethi <ishvindersethi22@gmail.com>
2020-10-08 18:34:33 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,5 @@
+pip3 install opencv-python
+pip3 install matplotlib
 pip3 install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install Pillow
 apt-get install libfreetype6-dev libharfbuzz-dev libfribidi-dev gtk-doc-tools
