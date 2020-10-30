360a4b69c686aa0b669f4360de256f6171ded5cb
Ishvinder Sethi <ishvindersethi22@gmail.com>
2020-10-30 10:27:29 +0530

    Update and rename requirements.sh to requirements.txt

--- requirements.sh
+++
@@ -1,9 +0,0 @@
-pip3 install opencv-python
-pip3 install matplotlib
-pip3 install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install Pillow
-apt-get install libfreetype6-dev libharfbuzz-dev libfribidi-dev gtk-doc-tools
-apt update
-apt install libraqm-dev
-ldconfig
-apt install fonts-indic
